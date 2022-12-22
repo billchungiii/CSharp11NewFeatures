f928e1510153a483830fea8059656d95554a7f90
Bill Chung <bill_chungiii@hotmail.com>
2022-12-23 01:24:13 +0800

    加入換行寫法

--- RawStringLiteralsSample/Program.cs
+++ RawStringLiteralsSample/Program.cs
@@ -11,7 +11,21 @@
             string value3 = """這有雙雙引號 ""在雙雙引號內""。""";
             Console.WriteLine(value3);
 
+            string longString1 = "長字串" + Environment.NewLine + "換行";
+            Console.WriteLine(longString1);
+            string longString2 = @"
+長字串
+換行";
+            Console.WriteLine(longString2);
 
+            string longString3 = """
+      長字串
+      換行
+      """;
+            Console.WriteLine(longString3);
+
+            string longString4 = $"長字串{Environment.NewLine}換行";
+            Console.WriteLine(longString4);
         }
     }
 }
